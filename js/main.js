42010d482bff65a885e890edbb5ca1e50c0ef60b
Bartek Świderski <bartekzg@gmail.com>
2024-06-14 14:54:42 +0200

    Poprawki baza danych

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
-import { setPopularMovie, eventHandler} from "./renderMovies.js";
-import {nextPage, pageOne} from './pagination.js'
-import { searchForm, gal, openModalBtn,closeModalBtn,modal, paginationBox,moddalWind,indexError} from "./utils.js";
-import {toggleModal,  selectId, fetchMovies, escape, renderModal} from "./modalLibrary.js"
-setPopularMovie()
-searchForm.addEventListener('submit', eventHandler);
-searchForm.addEventListener("input", ()=>{
+import { setPopularMovie, eventHandler } from "./renderMovies.js";
+import { nextPage, pageOne } from "./pagination.js";
+import {
+  searchForm,
+  gal,
+  openModalBtn,
+  closeModalBtn,
+  modal,
+  paginationBox,
+  moddalWind,
+  indexError,
+} from "./utils.js";
+import {
+  toggleModal,
+  selectId,
+  fetchMovies,
+  escape,
+  renderModal,
+} from "./modalLibrary.js";
+setPopularMovie();
+searchForm.addEventListener("submit", eventHandler);
+searchForm.addEventListener("input", () => {
   pageOne();
   indexError.style.display = "none";
-  while(paginationBox.firstChild){paginationBox.firstChild.remove()}
-  while(gal.firstChild){gal.firstChild.remove()}
- });
+  while (paginationBox.firstChild) {
+    paginationBox.firstChild.remove();
+  }
+  while (gal.firstChild) {
+    gal.firstChild.remove();
+  }
+});
 
-openModalBtn.addEventListener("click", async (event)=>{
+openModalBtn.addEventListener("click", async (event) => {
   let id = await selectId(event);
   let movie = await fetchMovies(id);
-  renderModal (movie);
-  toggleModal()
+  renderModal(movie);
+  toggleModal();
 });
-closeModalBtn.addEventListener("click", () => {while(moddalWind.firstChild){moddalWind.firstChild.remove()};
-  toggleModal()});
+closeModalBtn.addEventListener("click", () => {
+  while (moddalWind.firstChild) {
+    moddalWind.firstChild.remove();
+  }
+  toggleModal();
+});
 modal.addEventListener("click", escape());
-paginationBox.addEventListener("click", nextPage)
+paginationBox.addEventListener("click", nextPage);
--- js/modalLibrary.js
+++ js/modalLibrary.js
@@ -1,19 +1,34 @@
-import { moddalWind, imgURL, imgPlaceholder, modal, spinner, timeout, } from "./utils.js"
-import { addLocalStorageWached, addLocalStorageQueue } from "./addToLocalStorage.js";
+import {
+  moddalWind,
+  imgURL,
+  imgPlaceholder,
+  modal,
+  spinner,
+  timeout,
+} from "./utils.js";
+import {
+  addLocalStorageWached,
+  addLocalStorageQueue,
+} from "./addToLocalStorage.js";
+import { AddData, GetData, guid } from "./database.js";
 
 export function toggleModal() {
-    modal.classList.toggle("is-hidden");
-  }
+  modal.classList.toggle("is-hidden");
+}
 
 export function escape() {
-    while(moddalWind.firstChild){moddalWind.firstChild.remove()}
-    document.addEventListener("keydown", function (event) {
-      if (event.key === "Escape") {
-        while(moddalWind.firstChild){moddalWind.firstChild.remove()}
-        modal.classList.add("is-hidden");
-      }
-    });
+  while (moddalWind.firstChild) {
+    moddalWind.firstChild.remove();
   }
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      while (moddalWind.firstChild) {
+        moddalWind.firstChild.remove();
+      }
+      modal.classList.add("is-hidden");
+    }
+  });
+}
 
 export function selectId(event) {
   const id = event.target.id;
@@ -22,26 +37,35 @@ export function selectId(event) {
 
 export async function fetchMovies(id) {
   try {
-    await spinner.removeAttribute('hidden')
-    await timeout(300)
+    await spinner.removeAttribute("hidden");
+    await timeout(300);
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${id}?api_key=837f028f22fd2591f3672c74a92683e2&language=en-US`,
+      `https://api.themoviedb.org/3/movie/${id}?api_key=837f028f22fd2591f3672c74a92683e2&language=en-US`
     );
-    await spinner.setAttribute('hidden', '');
+    await spinner.setAttribute("hidden", "");
     if (!response.ok) {
       throw new Error(response.status);
     }
     return await response.json();
   } catch (err) {
     return console.log(err);
-  }};
+  }
+}
 
 export async function renderModal(movie) {
-let markupSec = ""
-const{poster_path, genres, vote_average, vote_count, popularity, original_title, overview} = movie;
-let gen = genres.map(genre => genre.name).join(", ")
-if(poster_path == null){;
-  markupSec=`
+  let markupSec = "";
+  const {
+    poster_path,
+    genres,
+    vote_average,
+    vote_count,
+    popularity,
+    original_title,
+    overview,
+  } = movie;
+  let gen = genres.map((genre) => genre.name).join(", ");
+  if (poster_path == null) {
+    markupSec = `
   <img class="moddal__poster" src="${imgPlaceholder}" alt="plakat" />
   <div>
   <h1 class="moddal__title">${original_title}</h1>
@@ -52,8 +76,9 @@ if(poster_path == null){;
   <h2 class="moddal__about">ABOUT</h2>
   <h3 class="moddal__aboutTxt">${overview}</h3>
   <div class="moddal__buttons"><button class="moddal__btn" watched>add to Watched</button><button class="moddal__btn" queue>add to queue</button></div>
-  </div>`}
-else {markupSec=`
+  </div>`;
+  } else {
+    markupSec = `
 <img class="moddal__poster" src="${imgURL}${poster_path}" alt="plakat" />
 <div>
 <h1 class="moddal__title">${original_title}</h1>
@@ -64,17 +89,29 @@ else {markupSec=`
 <h2 class="moddal__about">ABOUT</h2>
 <h3 class="moddal__aboutTxt">${overview}</h3>
 <div class="moddal__buttons"><button class="moddal__btn" watched>add to Watched</button><button class="moddal__btn" queue>add to queue</button></div>
-</div>`}
+</div>`;
+  }
   moddalWind.insertAdjacentHTML("beforeend", markupSec);
-  
+
   const modalButtonWatched = document.querySelector("button[watched]");
   const modalButtonQueue = document.querySelector("button[queue]");
-  
+
+  // modalButtonWatched.addEventListener("click", () => {
+  //   AddData("Watched", movie.id, guid);
+  // });
+
+  // modalButtonQueue.addEventListener("click", () => {
+  //   addLocalStorageQueue("queueLocalStorage", movie.id);
+  // });
+  // modalButtonWatched.addEventListener("click", () => {
+  //   GetData(guid, "Watched");
+  // });
+
   modalButtonWatched.addEventListener("click", () => {
-    addLocalStorageWached("watchedLocalStorage", movie.id);
+    AddData("Watched", movie.id, guid);
   });
 
   modalButtonQueue.addEventListener("click", () => {
-    addLocalStorageQueue("queueLocalStorage", movie.id);
+    AddData("Queue", movie.id, guid);
   });
 }
